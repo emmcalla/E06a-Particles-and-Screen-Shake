447ce27aaf47cab370cd4907da93201eabd4d4cc
emmcalla <59890244+emmcalla@users.noreply.github.com>
2020-02-19 11:55:23 -0500

    Exercise
    assignment complete

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,9 +1,20 @@
 extends Node2D
 
+export var acceleration = 0.1
+
 onready var Camera = $Camera
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("Left"):
+		velocity.x -= acceleration
+	if Input.is_action_pressed("Right"):
+		velocity.x += acceleration
+	if Input.is_action_pressed("Up"):
+		velocity.y -= acceleration
+	if Input.is_action_pressed("Down"):
+		velocity.y += acceleration
+(add_trauma(5))
+$Camera.add_trauma(0.1)
